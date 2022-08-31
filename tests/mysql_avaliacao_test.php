5ff77c2d8e17d5d941753e8f25d1c2973be7e24e
leticia <lefarias111@gmail.com>
2022-08-31 11:56:50 -0300

    blog

---
+++ tests/mysql_avaliacao_test.php
@@ -0,0 +1,86 @@
+<?php
+    //Aqui usamos o comando "require_once" para usar o código dos arquivos a seguir:
+    require_once '../includes/funcoes.php';
+    require_once '../core/conexao_mysql.php';
+    require_once '../core/sql.php';
+    require_once '../core/mysql.php';
+
+    //Aqui foi usado para fazer testes dos comandos de inserção, atualização, busca e exclusão de dados.
+    //insere dados
+    insert_teste('Leticia', 'leticia@ifsp.edu.br', '88856');
+    buscar_teste();
+    insert_teste2(2, 'Programando em PHP', 'linguagem', '2022-05-08');
+    buscar_teste2();
+    insert_teste3(2, 'nao aguento mais programar', 1, 1);
+    buscar_teste3();
+
+    //atualiza dados
+    update_teste(2, 'murilo', 'silva@gmail.com');
+    buscar_teste();
+    update_teste2(2, 'UHUUUUUUUU', 'lindeza');
+    buscar_teste2();
+    update_teste3(2, 10, 'nheu');
+    buscar_teste3();
+
+    //delete dados
+    delete_teste3(2);
+    buscar_teste3();
+    delete_teste2(2);
+    buscar_teste2();
+
+    //insert
+    function insert_teste($nome, $email, $senha) : void {
+        $dados = ['nome' => $nome, 'email' => $email, 'senha' => $senha];
+        insere('usuario', $dados);
+    }
+    function insert_teste2($usuario_id, $titulo, $texto, $data_postagem) : void {
+        $dados = ['usuario_id' => $usuario_id,'titulo' => $titulo, 'texto' => $texto, 'data_postagem' => $data_postagem];
+        insere('post', $dados);
+    }
+    function insert_teste3($nota, $comentario, $usuario_id, $post_id) : void {
+        $dados = ['nota' => $nota, 'comentario' => $comentario, 'usuario_id' => $usuario_id, 'post_id' => $post_id];
+        insere('avaliacao', $dados);
+    }
+
+    //buscar
+    function buscar_teste() : void {
+        $usuarios = buscar('usuario', ['id', 'nome', 'email', 'senha'], [], '');
+        print_r($usuarios);
+    }
+    function buscar_teste2() : void {
+        $usuarios = buscar('post', ['id', 'titulo', 'texto', 'data_postagem'], [], '');
+        print_r($usuarios);
+    }
+    function buscar_teste3() : void {
+        $usuarios = buscar('avaliacao', ['id', 'nota', 'comentario'], [], '');
+        print_r($usuarios);
+        echo "<br><hr>";
+    }
+
+    //updates
+    function update_teste($id, $nome, $email) : void {
+        $dados = ['nome' => $nome, 'email' => $email];
+        $criterio = [['id', '=', $id]];
+        atualiza('usuario', $dados, $criterio);
+    }
+    function update_teste2($id, $titulo, $texto) : void {
+        $dados = ['titulo' => $titulo, 'texto' => $texto];
+        $criterio = [['id', '=', $id]];
+        atualiza('post', $dados, $criterio);
+    }
+    function update_teste3($id, $nota, $comentario) : void {
+        $dados = ['nota' => $nota, 'comentario' => $comentario];
+        $criterio = [['id', '=', $id]];
+        atualiza('avaliacao', $dados, $criterio);
+    }
+
+    //delete
+    function delete_teste3($id) {
+        $criterio = [['id', '=', $id]];
+        deleta('avaliacao', $criterio);
+    }
+    function delete_teste2($id) {
+        $criterio = [['id', '=', $id]];
+        deleta('post', $criterio);
+    }
+?>
